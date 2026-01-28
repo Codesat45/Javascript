6eac614390a7a3ba3cec58236d67c93de36633db
Satyam Tripathi <141204829+Codesat45@users.noreply.github.com>
2026-01-28 14:25:35 +0000

    in this JS code we learned datatypes of JS

---
+++ Basic/third.js
@@ -0,0 +1,22 @@
+// "use strict"; //treat all JS code as newer vsersion
+// alert(3+3) --> it is because we are using node js  , not browser
+console.log(4-1)
+console.log(3+3)
+//number=> 2 to the power 53
+//bigint
+//string=>" "
+//null=>standalone value
+//boolean=>true/false
+//undefined=>value not assigned to the variable
+//symbol=>unique
+
+
+//object
+
+console.log(typeof "Satyam")
+console.log(typeof age)
+// console.log(typeof null)--> which throws in ouput that the datatype of null is object which is may or may not be the fault of JS.
+//console.log(typeof undefined)--> datatype of undefined is undefined
+
+
+
