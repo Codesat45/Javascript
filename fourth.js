2b2f69ee768d321b8388994a43dc326dc1e66c58
Satyam Tripathi <141204829+Codesat45@users.noreply.github.com>
2026-04-29 04:14:26 +0000

    typeconversion

---
+++ fourth.js
@@ -0,0 +1,9 @@
+let score =true;
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+let isLoggedIn = false;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); 
+let some="33abc";
+console.log(typeof(some));
